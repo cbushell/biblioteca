946ee1e90e11aa53905805de23d3c1f162fd2dc8
Chris Bushell <cbushell@thoughtworks.com>
2016-06-07 08:18:15 +0100

    simple main method

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -2,4 +2,16 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    public static void main(String[] args) {
+        Book[] books = new Book[]{
+                new Book("Charlie and the Chocolate Factory", "Roald Dahl", 1964),
+                new Book("The Catcher in the Rye", "J.D. Salinger", 1951)
+        };
+
+        Library library = new Library(books);
+        LibraryView libraryView = new LibraryView(library);
+        LibraryController libraryController = new LibraryController(library, libraryView);
+        libraryController.run();
+    }
+
 }
